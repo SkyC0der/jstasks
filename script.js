7ac5e4ea1b94ad2af798227312734d0501651844
Emmanuel Ezenwigbo <44367769+SkyC0der@users.noreply.github.com>
2020-03-22 18:48:27 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+let name = "Emmanuel Ezenwigbo";
+let courses = ["HTML", "CSS", "JavaScript", "PHP", "NodeJs" ];
+console.log(name);
+console.log(courses);
+let count = 1
+for(count; count <= 200; count+=2 ){
+    console.log(count);
+}
